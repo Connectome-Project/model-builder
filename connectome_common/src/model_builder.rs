38cb179b2491ca70e57374382dcffd20f0a8a4e9
Balazs Sevecsek <balazs.sevecsek@gmail.com>
2023-08-15 14:03:57 +0200

    Fix type errors

--- connectome_common/src/model_builder.rs
+++ connectome_common/src/model_builder.rs
@@ -1,26 +1,31 @@
+use std::cmp::Ordering;
 use std::error::Error;
+use std::marker::PhantomData;
 use std::str::Chars;
 
 use super::arc_model::NodeType;
 use super::arc_model::ThreadSafeModel;
 
-struct ModelBuilder<Mod, Dat, It, P>
+struct ModelBuilder<Mod, Dat, SomeInnerIterable, E, Pattern>
 where
-    P: Error,
-    It: Clone + Ord + 'static,
-    Dat: Iterator<Item = Result<It, P>>,
+    E: Error,
+    Pattern: Clone + Ord + 'static,
+    SomeInnerIterable: InnerIterable<Pattern>,
+    Dat: Iterator<Item = Result<SomeInnerIterable, E>>,
 {
     type_of: ModelBuilderType,
     config: TrainingConfig,
     model: Mod,
     data: Dat,
+    p: PhantomData<Pattern>,
 }
 
-impl<Mod, Dat, It, P> ModelBuilder<Mod, Dat, It, P>
+impl<Mod, Dat, SomeInnerIterable, E, Pattern> ModelBuilder<Mod, Dat, SomeInnerIterable, E, Pattern>
 where
-    P: Error,
-    It: Clone + Ord + 'static,
-    Dat: Iterator<Item = Result<It, P>>,
+    E: Error,
+    Pattern: Clone + Ord + 'static,
+    SomeInnerIterable: InnerIterable<Pattern>,
+    Dat: Iterator<Item = Result<SomeInnerIterable, E>>,
 {
     fn new(type_of: ModelBuilderType, config: TrainingConfig, model: Mod, data: Dat) -> Self {
         ModelBuilder {
@@ -28,20 +33,21 @@ where
             config,
             model,
             data,
+            p: PhantomData,
         }
     }
 }
 
-trait InnerIterable {
-    type Iterable<'a>
+trait InnerIterable<Item: Clone + Ord + 'static> {
+    type Iterable<'a>: Iterator<Item = Item>
     where
         Self: 'a;
 
     fn get_inner_iterable(&self) -> Self::Iterable<'_>;
 }
 
-impl InnerIterable for String {
-    type Iterable<'a> = Chars<'a>;
+impl InnerIterable<char> for String {
+    type Iterable<'a> = Chars<'a> where Self: 'a;
 
     fn get_inner_iterable(&self) -> Self::Iterable<'_> {
         self.chars()
@@ -59,28 +65,46 @@ pub enum ModelBuilderType {
 pub struct TrainingConfig {}
 
 trait ModelBuilderTrait<D> {
-    fn performAction(&self);
+    fn performAction(&mut self);
     fn isApplicable(&self) -> bool;
 }
 
-impl<'a, 'b, It, Additional, Dat, P> ModelBuilderTrait<Dat>
-    for ModelBuilder<ThreadSafeModel<'a, 'b, It, Additional>, Dat, It, P>
+impl<'a, 'b, SomeInnerIterable, Additional, Dat, E, DataPoint> ModelBuilderTrait<Dat>
+    for ModelBuilder<
+        ThreadSafeModel<'a, 'b, DataPoint, Additional>,
+        Dat,
+        SomeInnerIterable,
+        E,
+        DataPoint,
+    >
 where
-    P: Error,
-    It: Clone + Ord + 'static,
+    E: Error,
+    DataPoint: Clone + Ord + 'static,
     Additional: 'a + 'static,
-    Dat: Iterator<Item = Result<It, P>>,
+    SomeInnerIterable: InnerIterable<DataPoint>,
+    Dat: Iterator<Item = Result<SomeInnerIterable, E>>,
 {
     fn isApplicable(&self) -> bool {
         true
     }
-    fn performAction(&self) {
-        self.data.for_each(|x| {
-            let d = x.unwrap();
+    fn performAction(&mut self) {
+        for chunk in &mut self.data {
             let read = self.model.model.read().unwrap();
             let nodes = read.get_nodes();
             let start_nodes = nodes.iter().find(|y| y.node_type == NodeType::Start);
-        })
+
+            let chunk_res: SomeInnerIterable = chunk.unwrap();
+            let chunk_internal_iterator = chunk_res.get_inner_iterable();
+
+            for elem in chunk_internal_iterator {
+                let found_node: Option<&crate::arc_model::Node<DataPoint>> =
+                    nodes
+                        .iter()
+                        .find(|node: &&crate::arc_model::Node<DataPoint>| {
+                            node.pattern.cmp(&elem) == Ordering::Equal
+                        });
+            }
+        }
     }
 }
 
